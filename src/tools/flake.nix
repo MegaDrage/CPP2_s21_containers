4216178eed8223da2d9333e7f0260e584f5c0840
MegaDrage <dragonpsui@gmail.com>
2024-09-30 16:34:51 +0700

    add flake

---
+++ src/tools/flake.nix
@@ -0,0 +1,10 @@
+{
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs"; };
+  outputs = { self, nixpkgs }:
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        inputsFrom = [ self.packages.x86_64-linux.default ];
+      };
+    };
+}
